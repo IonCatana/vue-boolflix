c64c57b25fb151d0644eb90d31f5a116ddd20089
IonCatana <catana.ion17@yahoo.it>
2022-03-15 14:37:58 +0100

    get movie list from axios api url

--- src/components/MyMovies.vue
+++ src/components/MyMovies.vue
@@ -4,21 +4,40 @@
       <input v-model="search" type="text" />
       <button @click="searchMovie()">Search movie</button>
     </div>
+    <div class="movie" v-for="movie in movies" :key="movie.id">
+      <p>{{ movie.title }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
       search: "",
+      api_url:
+        "https://api.themoviedb.org/3/search/movie?api_key=2c70cf7212141e650767768ea94e23e6&language=en-US&query=matrix&page=1&include_adult=false",
+      movies: [],
     };
   },
   methods: {
     searchMovie() {
-      //console.log('cliccato sul button')
+      // console.log('cliccato sul button')
       console.log(this.search);
     },
+  },
+  mounted() {
+    axios
+      .get(this.api_url)
+      .then((response) => {
+        console.log(response);
+        this.movies = response.data.results;
+        console.log(this.movies);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   },
 };
 </script>
