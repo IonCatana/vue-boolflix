88d26c130ff6565f4d4704a993dbc3ea2e6070dd
IonCatana <catana.ion17@yahoo.it>
2022-03-15 14:22:43 +0100

    Add MYMovies component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyMovies />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MyMovies from "./components/MyMovies.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    MyMovies,
+  },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/MyMovies.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.container {
+  max-width: 1200px;
+  margin: 2rem auto;
+}
+</style>
