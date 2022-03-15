33195940bb3eb831f4dcaff5cc10525e80a955ea
IonCatana <catana.ion17@yahoo.it>
2022-03-15 16:13:52 +0100

    Milestone 2 finito

--- src/components/MyMovies.vue
+++ src/components/MyMovies.vue
@@ -2,17 +2,37 @@
   <div class="container">
     <div class="search_bar">
       <input v-model="search" type="text" />
-      <button @click="callApi(search)">Search movie</button>
+      <button @click="callApi(search)">Search</button>
     </div>
-    <div class="movie" v-for="movie in movies" :key="movie.id">
-      <div class="specifications_movie">
-        <h4>Title: {{ movie.title }}</h4>
-        <h5>Original title: {{ movie.original_title }}</h5>
-        <p>Language: {{ movie.original_language }}</p>
-        <p>Vote: {{ movie.vote_average }}</p>
+    <!-- /.search_bar -->
+    <div class="movies">
+      <div class="movie" v-for="movie in movies" :key="movie.id">
+        <div class="specifications_movie">
+          <h4>Title: {{ movie.title }} {{ movie.name }}</h4>
+          <h5>
+            Original title: {{ movie.original_title }} {{ movie.original_name }}
+          </h5>
+          <div class="languages">
+            <p>Language:</p>
+            <div class="flag">
+              <img
+                :src="movie.original_language"
+                :alt="movie.original_language"
+                width="25"
+              />
+            </div>
+          </div>
+          <!-- /.languages -->
+          <p>Vote: {{ movie.vote_average }}</p>
+        </div>
+        <!-- /.specifications_movie -->
       </div>
+      <!-- /.movie -->
+      <h5>{{ problem }}</h5>
     </div>
+    <!-- /.movies -->
   </div>
+  <!-- /.container -->
 </template>
 
 <script>
@@ -22,12 +42,43 @@ export default {
     return {
       search: "",
       movies: [],
+      problem: "",
+      flags: [
+        {
+          chiave: "en",
+          image: "https://images.emojiterra.com/twitter/512px/1f1ec-1f1e7.png",
+        },
+        {
+          chiave: "it",
+          image: "https://images.emojiterra.com/twitter/512px/1f1ee-1f1f9.png",
+        },
+        {
+          chiave: "de",
+          image:
+            "https://images.emojiterra.com/twitter/v13.1/512px/1f1e9-1f1ea.png",
+        },
+        {
+          chiave: "es",
+          image:
+            "https://images.emojiterra.com/twitter/v13.1/512px/1f1ea-1f1f8.png",
+        },
+        {
+          chiave: "fr",
+          image:
+            "https://images.emojiterra.com/twitter/v13.1/512px/1f1eb-1f1f7.png",
+        },
+      ],
+      countryCode: "",
+      status: "",
     };
   },
   methods: {
-    callApi(textInput) {
+    getMovie(textInput) {
       this.search = textInput;
-      console.log(this.search);
+      // console.log(this.search)
+    },
+    callApi() {
+      this.getMovie(this.search);
       axios
         // eslint-disable-next-line quotes
         .get(
@@ -36,15 +87,53 @@ export default {
             "&page=1&include_adult=false"
         )
         .then((response) => {
-          console.log(response);
+          // console.log(response)
           this.movies = response.data.results;
-          console.log(this.movies);
+          this.flagsLanguage(this.movies);
         })
         .catch((error) => {
-          console.log(error);
+          // console.log(error)
+          this.problem = error;
+          this.movies = "";
+          console.log(this.problem);
         });
+      axios
+        // eslint-disable-next-line quotes
+        .get(
+          "https://api.themoviedb.org/3/search/tv?api_key=2c70cf7212141e650767768ea94e23e6&language=en-US&page=1&include_adult=false&query=" +
+            this.search
+        )
+        .then((response) => {
+          // console.log(response)
+          this.movies = response.data.results;
+          this.flagsLanguage(this.movies);
+        })
+        .catch((error) => {
+          // console.log(error)
+          this.problem = error;
+          this.movies = "";
+          console.log(this.problem);
+        });
+      this.search = "";
+    },
+    flagsLanguage(listOfFlags) {
+      listOfFlags.forEach((flag) => {
+        this.iconsNations(flag);
+      });
+      return listOfFlags;
+    },
+    iconsNations(icon) {
+      this.flags.forEach((element) => {
+        console.log(element);
+        if (icon.original_language === element.chiave) {
+          icon.original_language = element.image;
+        }
+      });
     },
   },
+  /*  mounted: function () {
+    this.flagsLanguage()
+  } */
 };
 </script>
 
@@ -61,6 +150,13 @@ export default {
   }
   .movie {
     padding: 1rem;
+    .languages {
+      display: flex;
+      align-items: center;
+      & img {
+        margin-left: 0.5rem;
+      }
+    }
   }
 }
 </style>
